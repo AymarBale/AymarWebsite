9b9a54e5b5b034fc7671413ffcd3623e3333cf75
Nathanael BALE <62848857+AymarBale@users.noreply.github.com>
2023-12-06 12:09:30 -0500

    commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-function myFunction() {
-    /*console.log("test1");*/
-}
+let contactBut ;let contectSection;
+document.addEventListener('DOMContentLoaded', function() {
+    contactBut = document.getElementById('contactMeBut');
+    contectSection = document.getElementById('nav-8');
+    contactBut.addEventListener('click', function() {
+        // Scroll to the specific section (section2 in this case)
+        contectSection.scrollIntoView({ behavior: 'smooth' });
+    });
+});
 
