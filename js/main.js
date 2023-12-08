8fafe3c372b32b9f936b1141eef5da967544a994
Nathanael BALE <62848857+AymarBale@users.noreply.github.com>
2023-12-08 09:44:50 -0500

    test

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 let contactBut ;let contectSection;
 document.addEventListener('DOMContentLoaded', function() {
+    let taille = document.getElementById('taille');
+    taille.innerText = "width :"+window.innerWidth+"\n height :"+window.innerHeight;
     contactBut = document.getElementById('contactMeBut');
     contectSection = document.getElementById('nav-8');
     contactBut.addEventListener('click', function() {
