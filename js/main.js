94e24ab5e95c1466cb9f8be7d49132267696855b
Nathanael BALE <62848857+AymarBale@users.noreply.github.com>
2024-02-20 21:21:13 -0500

    m

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
-function openModal(title, description, imagePath, githubLink) {
+function openModal(title, description, imagePath, githubLink,langue) {
     var modal = document.getElementById('myModal');
     var modalTitle = document.getElementById('modalTitle');
     var modalDescription = document.getElementById('modalDescription');
     var modalImage = document.getElementById('modalImage');
     var githubLinkElement = document.getElementById('githubLink');
+    var language = document.getElementById('language');
 
     modalTitle.textContent = title;
     modalDescription.textContent = description;
     modalImage.src = imagePath;
     githubLinkElement.href = githubLink;
-
-    modal.style.display = 'block';
+    language.textContent = langue
+    modal.classList.add('show'); // Add the 'show' class to trigger the transition
 }
 
 function closeModal() {
     var modal = document.getElementById('myModal');
-    modal.style.display = 'none';
+    modal.classList.remove('show'); // Remove the 'show' class to trigger the transition
 }
+
